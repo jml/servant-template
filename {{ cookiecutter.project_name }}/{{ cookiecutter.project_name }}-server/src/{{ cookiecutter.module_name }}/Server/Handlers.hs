347c946a96a02e40f9b9d2073b64967a3cd66f4c
Jonathan Lange <jml@mumak.net>
2016-10-15 20:52:26 +0100

    Log using withFDHandler
    Introduces a new monad and natural monad transformer, setting the scene
    for #8.

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}-server/src/{{ cookiecutter.module_name }}/Server/Handlers.hs
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}-server/src/{{ cookiecutter.module_name }}/Server/Handlers.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeOperators #-}
 
 -- | Implementation of the {{ cookiecutter.project_name }} API.
 module {{ cookiecutter.module_name }}.Server.Handlers
@@ -10,22 +11,37 @@ module {{ cookiecutter.module_name }}.Server.Handlers
 -- Project.Server (which is command-line processing, setting up logs &
 -- monitoring, starting the HTTP server) should be in a different module.
 
-import Protolude
+import Protolude hiding (Handler)
 
-import Control.Monad.Log (MonadLog, Severity(..), WithSeverity)
-import Servant (Server, (:<|>)(..))
+import Control.Monad.Except (ExceptT(..))
+import Control.Monad.Log (Severity, logInfo)
+import Servant (ServantErr, Server, (:<|>)(..), (:~>)(..), enter)
+import Text.PrettyPrint.Leijen.Text (Doc, Pretty, text)
 
 import {{ cookiecutter.module_name }}.API (API, RootPage(..), User(..))
 import qualified {{ cookiecutter.module_name }}.Server.Logging as Log
 
 -- | {{ cookiecutter.project_name }} API implementation.
 server :: Severity -> Server API
-server logLevel = pure RootPage :<|> Log.withLogging logLevel users
+server logLevel = enter (toHandler logLevel) handlers
+  where
+    handlers = pure RootPage :<|> users
+
+-- | Our custom handler type.
+type Handler msg = ExceptT ServantErr (Log.LogM msg IO)
+
+-- | Translate our custom monad into a Servant handler.
+--
+-- See http://haskell-servant.readthedocs.io/en/stable/tutorial/Server.html#using-another-monad-for-your-handlers
+-- for the details.
+toHandler :: Pretty msg => Severity -> (Handler msg :~> ExceptT ServantErr IO)
+toHandler logLevel = Nat toHandler'
+  where
+    toHandler' :: Pretty msg => Handler msg a -> ExceptT ServantErr IO a
+    toHandler' = ExceptT . Log.withLogging logLevel . runExceptT
 
 -- | Example endpoint.
-users
-  :: (MonadIO m, MonadLog (WithSeverity LText) m)
-  => m [User]
+users :: Handler Doc [User]
 users = do
-  Log.log Informational ("Example of logging" :: LText)
+  logInfo (text "Example of logging")
   pure [User 1 "Isaac" "Newton", User 2 "Albert" "Einstein"]
--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}-server/src/{{ cookiecutter.module_name }}/Server/Logging.hs
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}-server/src/{{ cookiecutter.module_name }}/Server/Logging.hs
@@ -2,30 +2,33 @@
 
 -- | Logging helpers for {{ cookiecutter.project_name }}.
 module {{ cookiecutter.module_name }}.Server.Logging
-  ( withLogging
+  ( LogM
+  , withLogging
   , log
   , fromKeyword
   , toKeyword
   ) where
 
-import Protolude hiding (log)
+import Protolude hiding (Handler, log)
 
+import Control.Monad.Catch (MonadMask)
 import Control.Monad.Log
-       (LoggingT, MonadLog, Severity(..), WithTimestamp(..), WithSeverity(..),
-        logMessage, mapLogMessageM, renderWithSeverity,
-        renderWithTimestamp, runLoggingT, timestamp)
+       (Handler, LoggingT, MonadLog, Severity(..), WithTimestamp(..), WithSeverity(..),
+        defaultBatchingOptions, logMessage, mapLogMessageM, renderWithSeverity,
+        renderWithTimestamp, runLoggingT, timestamp, withFDHandler)
 import Data.Time.Format
        (defaultTimeLocale, formatTime, iso8601DateFormat)
-import System.IO (hFlush)
-import Text.PrettyPrint.Leijen.Text (Pretty(..))
+import Text.PrettyPrint.Leijen.Text (Doc, Pretty(..))
 
-type LogM msg m a = LoggingT (WithSeverity msg) (LoggingT (WithTimestamp (WithSeverity msg)) m) a
+type LogM msg m = LoggingT (WithSeverity msg) (LoggingT (WithTimestamp (WithSeverity msg)) m)
 
 -- | Take a bunch of logs with severity and print them to stdout with timestamps.
 withLogging
-  :: (MonadIO m, Pretty msg)
+  :: (MonadMask m, MonadIO m, Pretty msg)
   => Severity -> LogM msg m a -> m a
-withLogging severityThreshold body = runLoggingT (withTimestamps body) (printLogs severityThreshold)
+withLogging severityThreshold body =
+  withFDHandler defaultBatchingOptions stdout 0.4 80 $ \stdoutHandler ->
+    runLoggingT (withTimestamps body) (printLogs severityThreshold stdoutHandler)
 
 withTimestamps
   :: (MonadIO m, MonadLog (WithTimestamp msg) m)
@@ -58,11 +61,10 @@ toKeyword Debug = "debug"
 
 printLogs
   :: (Pretty a, MonadIO m)
-  => Severity -> WithTimestamp (WithSeverity a) -> m ()
-printLogs severityThreshold message =
-  when (severityThreshold >= msgSeverity (discardTimestamp message)) $ do
-    print . renderWithTimestamp timeFormatter (renderWithSeverity pretty) $ message
-    liftIO $ hFlush stdout
+  => Severity -> Handler m Doc -> WithTimestamp (WithSeverity a) -> m ()
+printLogs severityThreshold handler message =
+  when (severityThreshold >= msgSeverity (discardTimestamp message)) $
+    handler . renderWithTimestamp timeFormatter (renderWithSeverity pretty) $ message
   where
     timeFormatter = formatTime defaultTimeLocale timeFormat
     timeFormat = iso8601DateFormat (Just "%H:%M:%S.%q")
